855dc8499d8dc26230b25e27b63ca3f582b2e11b
Marcos Paulo <marcos@meucredere.com.br>
2023-01-20 16:36:01 -0300

    fix particle component rendering

--- lib/render_web/components/particle_component.ex
+++ lib/render_web/components/particle_component.ex
@@ -6,13 +6,11 @@ defmodule RenderWeb.ParticleComponent do
 
   def particle(assigns) do
     ~H"""
-    <%= for particle <- @particles do %>
-      <div style={"left: #{particle.x}%; top: #{particle.y}%;"} phx-click="delete" class="flex flex-1 h-1 w-1 absolute" phx-value-key={particle.key} phx-value-pid={particle.pid}>
+      <div id={@particle.pid} style={"left: #{@particle.x}%; top: #{@particle.y}%;"} phx-click="delete" class="flex flex-1 h-1 w-1 absolute" phx-value-key={@particle.key} phx-value-pid={@particle.pid}>
         <svg width="10" height="60">
             <rect width="10" height="60" style="fill:white" />
         </svg>
       </div>
-    <% end %>
     """
   end
 end
